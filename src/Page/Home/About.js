2c0e6c39bf0833b2f1fc1cadbd1c6f30d9b17f49
jasimahmedq2 <jasimahmed90852417>
2022-09-04 15:55:53 +0600

    Clean-co-ready

---
+++ src/Page/Home/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>this is about page details my work and information</h2>
+    </div>
+  );
+};
+
+export default About;
